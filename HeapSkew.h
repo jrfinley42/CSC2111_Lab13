7df1b9612409ae2f6266a956910c0f8ae7477f23
Jesse Robert Finley <jrfinley42@students.tntech.edu>
2017-04-20 09:48:23 -0500

    Fixed Spacing and touch production file

--- HeapSkew.h
+++ HeapSkew.h
@@ -8,23 +8,23 @@ template < class T >
 class HeapSkew : public Drawable
 {
 
-   private:
-      BinaryTree<T>* bt;
-      int sze;
-      BinaryTree<T>* merge(BinaryTree<T>* left, BinaryTree<T>* right);
+	private:
+		BinaryTree<T>* bt;
+		int sze;
+		BinaryTree<T>* merge(BinaryTree<T>* left, BinaryTree<T>* right);
 
-      int (*compare_items) (T* item_1, T* item_2);
+		int (*compare_items) (T* item_1, T* item_2);
 
-   public:
-      HeapSkew(int (*comp_items) (T* item_1, T* item_2));
-      ~HeapSkew();
+	public:
+		HeapSkew(int (*comp_items) (T* item_1, T* item_2));
+		~HeapSkew();
 
-      bool heapIsEmpty();
-      void heapInsert(T* item);
-      T* heapRemove();
+		bool heapIsEmpty();
+		void heapInsert(T* item);
+		T* heapRemove();
 
-      void draw(wxDC& dc, int width, int height);
-      void mouseClicked(int x, int y);
+		void draw(wxDC& dc, int width, int height);
+		void mouseClicked(int x, int y);
 
 };
 
@@ -32,22 +32,22 @@ class HeapSkew : public Drawable
 template < class T >
 HeapSkew<T>::HeapSkew(int (*comp_items) (T* item_1, T* item_2)) : Drawable()
 { 
-   bt = new BinaryTree<T>();
-   sze = 0;
+	bt = new BinaryTree<T>();
+	sze = 0;
 
-   compare_items = comp_items;
+	compare_items = comp_items;
 }
 
 template < class T >
 HeapSkew<T>::~HeapSkew() 
 { 
-   delete bt;
+	delete bt;
 }  
 
 template < class T >
 bool HeapSkew<T>::heapIsEmpty()
 {
-   return sze == 0;
+	return sze == 0;
 }
 
 template < class T >
@@ -73,7 +73,7 @@ BinaryTree<T>* HeapSkew<T>::merge(BinaryTree<T>* left, BinaryTree<T>* right)
 template < class T >
 void HeapSkew<T>::heapInsert(T* item)
 {
-   //DO THIS (calls merge, should be short)
+	//DO THIS (calls merge, should be short)
 
 
 
@@ -83,7 +83,7 @@ void HeapSkew<T>::heapInsert(T* item)
 template < class T >
 T* HeapSkew<T>::heapRemove()
 {
-   //DO THIS (calls merge, should be short)
+	//DO THIS (calls merge, should be short)
 
 
 
@@ -95,7 +95,7 @@ T* HeapSkew<T>::heapRemove()
 template < class T >
 void HeapSkew<T>::draw(wxDC& dc, int width, int height)
 {
-   bt->draw(dc, width, height);
+	bt->draw(dc, width, height);
 }
 
 template < class T >
